e8e020eff1e6e5ae40afe57fc73bd7105ebb356d
Uldez89 <magdeev89@mail.ru>
2023-06-19 15:02:41 +0300

    домашняя работа массивы

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,14 +1,12 @@
 function compareArrays(arr1, arr2) {
-    function compareArrays(arr1, arr2) {
-        return arr1.length === arr2.length && arr1.every((items, index) => items === arr2[index]);
-    } 
+    return arr1.length === arr2.length && arr1.every((items, index) => items === arr2[index]);   
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-    let result = people.filter(people => people.gender === gender).map(people => people.age).reduce((acc, item, index, people) => {
+    let result = users.filter(users => users.gender === gender).map(users => users.age).reduce((acc, item, index, users) => {
       acc += item;
-      if ( index === people.length - 1){
-        return acc / people.length;
+      if ( index === users.length - 1){
+        return acc / users.length;
       } 
       return acc;
     }, 0);
