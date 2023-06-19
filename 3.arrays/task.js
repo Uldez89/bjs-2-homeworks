16bb6a17025e7199130d7d80197eaef287b029d8
Uldez89 <magdeev89@mail.ru>
2023-06-19 13:42:35 +0300

    Первая версия Массивы

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,16 @@
 function compareArrays(arr1, arr2) {
-  
+    function compareArrays(arr1, arr2) {
+        return arr1.length === arr2.length && arr1.every((items, index) => items === arr2[index]);
+    } 
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
-}
+    let result = people.filter(people => people.gender === gender).map(people => people.age).reduce((acc, item, index, people) => {
+      acc += item;
+      if ( index === people.length - 1){
+        return acc / people.length;
+      } 
+      return acc;
+    }, 0);
+    return result;
+  }
