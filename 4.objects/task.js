7c5cd3079d703f534ae488ae4da75ec2c53fae07
Uldez89 <magdeev89@mail.ru>
2023-06-22 23:41:48 +0300

    домашняя работа объекты

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,37 @@
 function Student(name, gender, age) {
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
   
 }
+let student1 = new Student('Petr', 'men', 33);
+let student2 = new Student('Ann', 'women', 31);
+let student3 = new Student('Viktor', 'men', 38);
 
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function(...marksToAdd) {
+        this.marks = [...this.marks, ...marksToAdd];
 }
 
 Student.prototype.getAverage = function () {
-  
+    if (typeof this.marks === false) {
+        return 0;
+    }
+    let result = this.marks.reduce((acc, item, index, arr) => {
+        acc += item;
+        if (index === arr.length - 1) {
+            return acc / arr.length
+        } return acc;
+    }, 0);
+    return result;
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
 }
