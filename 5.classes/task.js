de3e5d094cac7aab95da7b18d03ce571b1c6c45e
Uldez89 <magdeev89@mail.ru>
2023-06-28 23:32:18 +0300

    домашняя работа классы

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -16,7 +16,7 @@ class PrintEditionItem {
 
         if (value < 0) {
             this._state = 0;
-        } else if (value > 0) {
+        } else if (value > 100) {
             this._state = 100;
         } else {
             this._state = value;
@@ -26,5 +26,70 @@ class PrintEditionItem {
     get state() {
         return this._state;
     }
-
 }
+
+class Magazine extends PrintEditionItem {
+    constructor(name, releaseDate, pagesCount) {
+        super(name, releaseDate, pagesCount);
+        this.type = "magazine";
+    }
+}
+
+class Book extends Magazine {
+    constructor(author, name, releaseDate, pagesCount) {
+        super(name, releaseDate, pagesCount);
+        this.type = "book";
+        this.author = author;
+    }
+}
+
+class NovelBook extends Book {
+    constructor(name, author, releaseDate, pagesCount) {
+        super(name, releaseDate, pagesCount);
+        this.type = "novel";
+    }
+}
+
+class FantasticBook extends Book {
+    constructor(name, author, releaseDate, pagesCount) {
+        super(name, releaseDate, pagesCount);
+        this.type = "fantastic";
+    }
+}
+
+class DetectiveBook extends Book {
+    constructor(name, author, releaseDate, pagesCount) {
+        super(name, releaseDate, pagesCount);
+        this.type = "detective";
+    }
+}
+
+class Library {
+    constructor(name) {
+        this.name = name;
+        this.books = [];
+    }
+
+    addBook(book) {
+        if (book.state > 30) {
+            return this.books.push(book);
+        }
+    }
+
+    findBookBy(type, value) {
+        let result = books.find(book => {
+            if (['type'] in book && book.hasOwnProperty(.['value']) {
+                return book;
+            } else {
+                return null;
+            }
+        });
+        return result;
+    }
+
+    giveBookByName(bookName) {
+        // let result = books.find(book => book.name === bookName));
+        // return result || null;
+    }
+}
+
