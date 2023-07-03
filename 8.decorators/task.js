9429781565d8bc9948725bad2b54cb43758ff1b0
Uldez89 <magdeev89@mail.ru>
2023-07-03 23:39:07 +0300

    домашняя работа Декораторы

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,42 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
+    let cache = {};
+    return (...args) => {
+        const hash = md5(args);
+        if (hash in cache) {
+            return 'Из кэша: ' + cache[hash];
+        }
+        const result = func(...args);
+        if (Object.keys(cache).length > 4) {
+            cache[hash] = result;
+            let propName = Object.keys(cache)[0];
+            delete cache[propName];
+            return 'Вычисляем: ' + result;
+        }
+        cache[hash] = result;
+        return 'Вычисляем: ' + result;
+    }
 }
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
-  
+    let timerId;
+    function resultFunction(...args) {
+        resultFunction.allCount++;
+
+        if (!timerId) {
+            func(...args);
+            resultFunction.count++;
+        }
+        
+        clearTimeout(timerId);
+
+        timerId = setTimeout(() => {
+            func(...args);
+            resultFunction.count++;
+        }, delay)
+    }
+    resultFunction.count = 0;
+    resultFunction.allCount = 0;
+    return resultFunction;
 }
